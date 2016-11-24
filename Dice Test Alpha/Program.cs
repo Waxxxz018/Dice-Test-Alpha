b2c1119599fc2ac582a978526a010ee157c0dedc
Waxxxz018 <toxiccrusader018@att.net>
2016-11-23 18:22:46 -0800

    Minor changes...

--- Dice Test Alpha/Program.cs
+++ Dice Test Alpha/Program.cs
@@ -1,101 +1,101 @@
-﻿using System;
+using System;
 using System.Security.Cryptography;
+
 namespace Dice_Test_Alpha
 {
     internal class Program
     {
         private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
-        public enum Dice : uint
+
+        public enum Dice
         {
             /// <summary>
             /// This can be considered a double-sided coin;
             /// used to delimit a 50/50 probability.
             /// </summary>
             D2 = 2,
+
             /// <summary>
             /// A Tetrahedron
             /// A 4 Sided Die
             /// </summary>
             D4 = 4,
+
             /// <summary>
             /// A Cube
             /// A 6 Sided Die
             /// </summary>
             D6 = 6,
+
             /// <summary>
             /// A Octahedron
             /// A 8 Sided Die
             /// </summary>
             D8 = 8,
+
             /// <summary>
             /// A Pentagonal Trapezohedron
             /// A 10 Sided Die
             /// </summary>
             D10 = 10,
+
             /// <summary>
             /// A Dodecahedron
             /// A 12 Sided Die
             /// </summary>
             D12 = 12,
+
             /// <summary>
             /// A Icosahedron
             /// A 20 Sided Die
             /// </summary>
             D20 = 20,
+
             /// <summary>
             /// A Rhombic Triacontahedron
             /// A 30 Sided Die
             /// </summary>
             D30 = 30,
+
             /// <summary>
             /// A Icosakaipentagonal Trapezohedron
             /// A 50 Sided Die
             /// </summary>
             D50 = 50,
+
             /// <summary>
             /// A Pentagonal Hexecontahedron
             /// A 60 Sided Die
             /// </summary>
             D60 = 60,
+
             /// <summary>
             /// A Zocchihedron
             /// A 100 Sided Die
             /// </summary>
             D100 = 100
         };
-        public static byte RollDice(byte numberSides)
+
+        public static byte RollDie(int numberofDice, byte numberSides)
         {
+            if (numberofDice <= 0)
+                throw new ArgumentOutOfRangeException("numberofDice");
+
             if (numberSides <= 0)
                 throw new ArgumentOutOfRangeException("numberSides");
+
             // Create a byte array to hold the random value.
             byte[] randomNumber = new byte[1];
-            do
-            {
-                // Fill the array with a random value.
-                rngCsp.GetBytes(randomNumber);
-            }
-            while (!IsFairRoll(randomNumber[0], numberSides));
-            // Return the random number mod the number
-            // of sides.  The possible values are zero-
-            // based, so we add one.
+
+            // Fill the array with a random value.
+            rngCsp.GetBytes(randomNumber);
+
             return (byte)((randomNumber[0] % numberSides) + 1);
         }
-        private static bool IsFairRoll(byte roll, byte numSides)
-        {
-            // There are MaxValue / numSides full sets of numbers that can come up
-            // in a single byte.  For instance, if we have a 6 sided die, there are
-            // 42 full sets of 1-6 that come up.  The 43rd set is incomplete.
-            int fullSetsOfValues = Byte.MaxValue / numSides;
-            // If the roll is within this range of fair values, then we let it continue.
-            // In the 6 sided die case, a roll between 0 and 251 is allowed.  (We use
-            // < rather than <= since the = portion allows through an extra 0 value).
-            // 252 through 255 would provide an extra 0, 1, 2, 3 so they are not fair
-            // to use.
-            return roll < numSides * fullSetsOfValues;
-        }
+
         private static void Main(string[] args)
         {
-            var r = RollDice(Convert.ToByte(Dice.D2));
+            var r = RollDie(1, Convert.ToByte(Dice.D20));
             Console.WriteLine(r);
             Console.ReadLine();
         }
